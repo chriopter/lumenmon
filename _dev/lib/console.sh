24c93b0e9d1607da38970f2e42664562c050b233
Christopher <ce@mail101.de>
2025-09-20 15:39:24 +0200

    adjust dev scripts

--- _dev/lib/console.sh
+++ _dev/lib/console.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Start console container
 
+PROJECT_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 echo "Starting console..."
-cd ../console && docker compose up -d --build
+cd "$PROJECT_ROOT/console" && docker compose up -d --build
