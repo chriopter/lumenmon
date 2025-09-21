9e675dfe00977599941c1c05234ebef5513bfd87
chriopter <82179548+chriopter@users.noreply.github.com>
2025-09-21 14:09:13 +0200

    commit

--- installer/menu.sh
+++ installer/menu.sh
@@ -33,70 +33,51 @@ draw_menu() {
     echo -e "  \033[0;90m[↑↓ Navigate, Enter Select, q Quit]\033[0m"
 }
 
-# Get single keypress
-get_key() {
-    local key
-    # Read from /dev/tty if available (for piped execution)
-    if [ -t 0 ]; then
-        IFS= read -rsn1 key
-    else
-        IFS= read -rsn1 key < /dev/tty
-    fi
-
-    if [[ $key = $'\x1b' ]]; then
-        # Read arrow key sequence
-        if [ -t 0 ]; then
-            read -rsn2 key
-        else
-            read -rsn2 key < /dev/tty
-        fi
-        case $key in
-            '[A') echo UP ;;
-            '[B') echo DOWN ;;
-        esac
-    else
-        echo "$key"
-    fi
-}
-
 # Main menu
 show_menu() {
     local options=("Install Console" "Advanced" "Exit")
     local selected=0
 
+    # Ensure we're reading from terminal
+    exec < /dev/tty
+
     while true; do
         draw_menu $selected "${options[@]}"
 
-        key=$(get_key)
-        case $key in
-            UP)
-                ((selected--))
-                [ $selected -lt 0 ] && selected=$((${#options[@]} - 1))
-                ;;
-            DOWN)
-                ((selected++))
-                [ $selected -ge ${#options[@]} ] && selected=0
-                ;;
-            q|Q)
-                exit 0
-                ;;
-            '')  # Enter key
-                case $selected in
-                    0)  # Install Console
-                        COMPONENT="console"
-                        IMAGE=""
-                        break
-                        ;;
-                    1)  # Advanced
-                        show_advanced
-                        break
-                        ;;
-                    2)  # Exit
-                        exit 0
-                        ;;
-                esac
-                ;;
-        esac
+        # Read a single character
+        read -rsn1 key
+
+        # Check for escape sequence (arrow keys)
+        if [[ $key == $'\x1b' ]]; then
+            read -rsn2 -t 0.1 key
+            case $key in
+                '[A') # Up arrow
+                    ((selected--))
+                    [ $selected -lt 0 ] && selected=$((${#options[@]} - 1))
+                    ;;
+                '[B') # Down arrow
+                    ((selected++))
+                    [ $selected -ge ${#options[@]} ] && selected=0
+                    ;;
+            esac
+        elif [[ $key == "" ]]; then # Enter key
+            case $selected in
+                0)  # Install Console
+                    COMPONENT="console"
+                    IMAGE=""
+                    break
+                    ;;
+                1)  # Advanced
+                    show_advanced
+                    break
+                    ;;
+                2)  # Exit
+                    exit 0
+                    ;;
+            esac
+        elif [[ $key == "q" ]] || [[ $key == "Q" ]]; then
+            exit 0
+        fi
     done
 }
 
@@ -116,58 +97,60 @@ show_advanced() {
     while true; do
         draw_menu $selected "${options[@]}"
 
-        key=$(get_key)
-        case $key in
-            UP)
-                ((selected--))
-                [ $selected -lt 0 ] && selected=$((${#options[@]} - 1))
-                ;;
-            DOWN)
-                ((selected++))
-                [ $selected -ge ${#options[@]} ] && selected=0
-                ;;
-            q|Q)
-                exit 0
-                ;;
-            '')  # Enter key
-                case $selected in
-                    0)  # Console - Latest
-                        COMPONENT="console"
-                        IMAGE="ghcr.io/chriopter/lumenmon-console:latest"
-                        break
-                        ;;
-                    1)  # Console - Dev
-                        COMPONENT="console"
-                        IMAGE="ghcr.io/chriopter/lumenmon-console:main"
-                        break
-                        ;;
-                    2)  # Console - Build local
-                        COMPONENT="console"
-                        IMAGE=""
-                        break
-                        ;;
-                    3)  # Agent - Latest
-                        COMPONENT="agent"
-                        IMAGE="ghcr.io/chriopter/lumenmon-agent:latest"
-                        break
-                        ;;
-                    4)  # Agent - Dev
-                        COMPONENT="agent"
-                        IMAGE="ghcr.io/chriopter/lumenmon-agent:main"
-                        break
-                        ;;
-                    5)  # Agent - Build local
-                        COMPONENT="agent"
-                        IMAGE=""
-                        break
-                        ;;
-                    6)  # Back
-                        show_menu
-                        break
-                        ;;
-                esac
-                ;;
-        esac
+        # Read a single character
+        read -rsn1 key
+
+        # Check for escape sequence (arrow keys)
+        if [[ $key == $'\x1b' ]]; then
+            read -rsn2 -t 0.1 key
+            case $key in
+                '[A') # Up arrow
+                    ((selected--))
+                    [ $selected -lt 0 ] && selected=$((${#options[@]} - 1))
+                    ;;
+                '[B') # Down arrow
+                    ((selected++))
+                    [ $selected -ge ${#options[@]} ] && selected=0
+                    ;;
+            esac
+        elif [[ $key == "" ]]; then # Enter key
+            case $selected in
+                0)  # Console - Latest
+                    COMPONENT="console"
+                    IMAGE="ghcr.io/chriopter/lumenmon-console:latest"
+                    break
+                    ;;
+                1)  # Console - Dev
+                    COMPONENT="console"
+                    IMAGE="ghcr.io/chriopter/lumenmon-console:main"
+                    break
+                    ;;
+                2)  # Console - Build local
+                    COMPONENT="console"
+                    IMAGE=""
+                    break
+                    ;;
+                3)  # Agent - Latest
+                    COMPONENT="agent"
+                    IMAGE="ghcr.io/chriopter/lumenmon-agent:latest"
+                    break
+                    ;;
+                4)  # Agent - Dev
+                    COMPONENT="agent"
+                    IMAGE="ghcr.io/chriopter/lumenmon-agent:main"
+                    break
+                    ;;
+                5)  # Agent - Build local
+                    COMPONENT="agent"
+                    IMAGE=""
+                    break
+                    ;;
+                6)  # Back
+                    show_menu
+                    break
+                    ;;
+            esac
+        fi
     done
 }
 
